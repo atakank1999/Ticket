cd19c133301f438411725af16a65f3fbd869a129
Atakan <atakank1999@hotmail.com>
2020-09-03 03:18:26 +0300

    add reminder emails

--- Ticket/Models/Automation/Email.cs
+++ Ticket/Models/Automation/Email.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace Ticket.Models.Automation
@@ -12,18 +14,13 @@ namespace Ticket.Models.Automation
         public void run_cmd(string cmd, string args)
         {
             ProcessStartInfo start = new ProcessStartInfo();
-            start.FileName = "D:/Python34/python.exe";
+            start.FileName = "C:/Python/python.exe";
             start.Arguments = string.Format("{0} {1}", cmd, args);
             start.UseShellExecute = false;
             start.RedirectStandardOutput = true;
-            using (Process process = Process.Start(start))
-            {
-                using (StreamReader reader = process.StandardOutput)
-                {
-                    string result = reader.ReadToEnd();
-                    Console.Write(result);
-                }
-            }
+            Process.Start(start);
+
         }
+        
     }
 }
